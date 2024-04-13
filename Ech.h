e211cfebe01fe01d5e78463b256d4c5e3a3c0eee
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-04-14 01:54:30 +0700

    new

---
+++ Ech.h
@@ -0,0 +1,29 @@
+#ifndef ECH_H_
+#define ECH_H_
+
+#include "BaseObject.h"
+
+#define JUMP 1
+#define FALL 2
+#define RUSH 3
+#define RUN	0
+
+class Character
+{
+public:
+    static const int NHAY_SPEED = 8;
+    static const int ROI_SPEED = 8;
+
+    Character();
+
+    bool TrenMatDat();
+    void Event(SDL_Event &e, Mix_Chunk *g_Jump);
+    void DiChuyen();
+    void Render(SDL_Rect *currentClip, SDL_Renderer *g_Renderer, BaseObject g_CharacterTexture);
+    int GetPosX();
+    int GetPosY();
+
+private:
+    int posX, posY, status;
+};
+#endif // ECH_H_INCLUDED
