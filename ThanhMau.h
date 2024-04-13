77d1c0d3fc6b419c1a031d30ee8dae4898a0ea8e
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-04-14 01:56:04 +0700

    new

---
+++ ThanhMau.h
@@ -0,0 +1,20 @@
+#ifndef THANHMAU_H_
+#define THANHMAU_H_
+
+#include "BaseObject.h"
+
+class HealthBar
+{
+public:
+    HealthBar();
+
+    void Render(SDL_Rect* currentClip, SDL_Renderer* g_Renderer, BaseObject g_HealthBarTexture );
+
+    int GetPosX();
+    int GetPosY();
+private:
+    int PosX, PosY;
+    int status;
+};
+
+#endif // THANHMAU_H_
