892eb6670bf129d5989acc32fa8c7a199d0f9684
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-04-17 11:18:57 +0700

    update1

--- HoiMau.h
+++ HoiMau.h
@@ -1,6 +1,31 @@
 #ifndef HOIMAU_H_
 #define HOIMAU_H_
 
+#include "BaseObject.h"
 
-const
+#define APPLE_POSITION_RANGE 250
+
+using namespace std;
+
+class Apple
+{
+public:
+    Apple();
+    ~Apple();
+
+    void LoadFromFile(string path, SDL_Renderer* g_Renderer );
+    void Move( const int& acceleration );
+    void Render ( SDL_Renderer* g_Renderer, SDL_Rect* currentClip = nullptr );
+
+    int Speed( const int& acceleration );
+    int GetPosX();
+    int GetPosY();
+    int GetWidth();
+    int GetHeight();
+
+private:
+    int PosX, PosY;
+    int e_Width, e_Height;
+    SDL_Texture *AppleTexture;
+};
 #endif // HOIMAU_H_
