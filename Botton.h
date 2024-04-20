e4d6b8a93ccb3130ee87d7c4c0d4b3601929fc38
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2024-04-21 01:15:33 +0700

    moi1

---
+++ Botton.h
@@ -0,0 +1,26 @@
+#ifndef BOTTON_H_
+#define BOTTON_H_
+
+#include "BaseObject.h"
+#include "Func.h"
+
+class Button
+{
+public:
+    ButtonSprite currentSprite;
+
+    Button();
+
+    Button(int x, int y);
+
+    void SetPosition(int x, int y);
+
+    bool IsInside(SDL_Event *e, int size);
+
+    void Render(SDL_Rect* currentClip, SDL_Renderer* g_Renderer, BaseObject g_ButtonTexture);
+
+private:
+    SDL_Point position;   //Cau truc diem(x,y)
+};
+
+#endif // BOTTON_H_INCLUDED
